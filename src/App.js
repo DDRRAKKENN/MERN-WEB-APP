976f6a0118fdfcf12f187fc9d53285f9fdbd8b69
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2021-02-26 08:49:57 -0300

    SASS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>MERN Web personal</h1>
+      <h2>proyecto</h2>
     </div>
   );
 }
